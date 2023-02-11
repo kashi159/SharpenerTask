3ed70c2f16668d8277c26b1311fe157766cb5fde
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-11 15:27:39 +0530

    commit

--- Advance_jS/break_promise.js
+++ Advance_jS/break_promise.js
@@ -34,32 +34,30 @@ function deletePost(){
 
 //Modify the lines below and use .then and .catch smartly to get the correct output.
 //1st method
-create2ndPost().then(()=>{
-    deletePost().then((deletedPost1)=>{
-        console.log(deletedPost1.title)
-        create3rPost().then(()=>{
-            deletePost().then((deletedPost2)=>{
-                console.log(deletedPost2.title)
-                deletePost().then((deletedPost3)=>{
-                    console.log(deletedPost3.title)
-                    deletePost().catch((msg)=>{
-                        console.log(msg)
-                    })
-                })
-            })
-        })
-    })
-})
+// create2ndPost().then(()=>{
+//     deletePost().then((deletedPost1)=>{
+//         console.log(deletedPost1.title)
+//         create3rPost().then(()=>{
+//             deletePost().then((deletedPost2)=>{
+//                 console.log(deletedPost2.title)
+//                 deletePost().then((deletedPost3)=>{
+//                     console.log(deletedPost3.title)
+//                     deletePost().catch((msg)=>{
+//                         console.log(msg)
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
 
 //2nd method
 
-// create2ndPost().then(()=>{return deletePost()})
-// .then((deletedPost2)=>{console.log(deletedPost2.title)})
-// .then(()=>{ return create3rPost() })
-// .then(()=>{ return deletePost()})
-// .then((deletedPost3)=>{console.log(deletedPost3.title)})
-// .then(()=>{ return deletePost()})
-// .then((deletedPost1)=>{console.log(deletedPost1.title)})
-// .then(()=>{ return deletePost()})
-// .catch((msg)=>{console.log(msg)})
+create2ndPost()
+.then(()=>{ return deletePost().then((deletedPost2)=>{console.log(deletedPost2.title)}) })
+.then(()=>{ return create3rPost() })
+.then(()=>{ return deletePost().then((deletedPost3)=>{console.log(deletedPost3.title)}) })
+.then(()=>{ return deletePost().then((deletedPost1)=>{console.log(deletedPost1.title)}) })
+.then(()=>{ return deletePost()})
+.catch((msg)=>{console.log(msg)})
             
