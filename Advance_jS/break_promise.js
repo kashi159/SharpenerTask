52dc8f0e213efbc382be0ba86abaa9c4c00bedf5
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-11 15:37:14 +0530

    changes

--- Advance_jS/break_promise.js
+++ Advance_jS/break_promise.js
@@ -54,10 +54,10 @@ function deletePost(){
 //2nd method
 
 create2ndPost()
-.then(()=>{ return deletePost().then((deletedPost2)=>{console.log(deletedPost2.title)}) })
+.then(()=>{ return deletePost().then(deletedPost2 => console.log(deletedPost2.title)) })
 .then(()=>{ return create3rPost() })
-.then(()=>{ return deletePost().then((deletedPost3)=>{console.log(deletedPost3.title)}) })
-.then(()=>{ return deletePost().then((deletedPost1)=>{console.log(deletedPost1.title)}) })
+.then(()=>{ return deletePost().then(deletedPost3 => console.log(deletedPost3.title)) })
+.then(()=>{ return deletePost().then(deletedPost1 => console.log(deletedPost1.title)) })
 .then(()=>{ return deletePost()})
-.catch((msg)=>{console.log(msg)})
+.catch(err => console.log(err))
             
