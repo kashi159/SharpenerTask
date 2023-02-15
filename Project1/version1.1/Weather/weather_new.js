d027c88125ea049b7991152d1a61694c918ce535
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-15 21:59:15 +0530

    Added Search city option

--- Project1/version1.1/Weather/weather_new.js
+++ Project1/version1.1/Weather/weather_new.js
@@ -1,47 +1,51 @@
 const apiKey = 'aca8cb12e2f477338eec7f9249864994'; // Replace with your OpenWeatherMap API key
 const pexelsApiKey = 'JyOgdRiWx2YdKgyZLqkYlOgDrGtMqNkBqStOg0WswuSWd1eN7eUbQMFq'; // Replace with your Pexels API key
-const city = 'Darbhanga';
-const state = 'Bihar';
-const country = 'IN';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${apiKey}`;
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const weatherContainer = document.getElementById('weather-container');
+const errorContainer = document.getElementById('error-container');
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const temperature = data.main.temp - 273.15; // Convert from Kelvin to Celsius
-    const weatherDescription = data.weather[0].description;
-    const weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed;
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const city = cityInput.value.trim();
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-    // Search for background image on Pexels based on weather conditions
-    const weatherKeywords = ['clear', 'cloud', 'rain', 'snow'];
-    let query = 'nature'; // Default query
-    for (let i = 0; i < weatherKeywords.length; i++) {
-      if (weatherDescription.includes(weatherKeywords[i])) {
-        query = weatherKeywords[i];
-        break;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const temperature = data.main.temp - 273.15; // Convert from Kelvin to Celsius
+      const weatherDescription = data.weather[0].description;
+      const weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+
+      // Search for background image on Pexels based on weather conditions
+      const weatherKeywords = ['clear', 'cloud', 'rain', 'snow', 'smoke', 'mist', 'haze'];
+      let query = 'nature'; // Default query
+      for (let i = 0; i < weatherKeywords.length; i++) {
+        if (weatherDescription.includes(weatherKeywords[i])) {
+          query = weatherKeywords[i];
+          break;
+        }
       }
-    }
 
-    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${query}&per_page=1`;
-    fetch(pexelsApiUrl, { headers: { Authorization: pexelsApiKey } })
-      .then(response => response.json())
-      .then(data => {
-        const backgroundImage = data.photos[0].src.large;
+      const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${query}&per_page=1`;
+      fetch(pexelsApiUrl, { headers: { Authorization: pexelsApiKey } })
+        .then(response => response.json())
+        .then(data => {
+          const backgroundImage = data.photos[0].src.large;
 
-        // Set background image
-        document.body.style.backgroundImage = `url(${backgroundImage})`;
+          // Set background image
+          document.body.style.backgroundImage = `url(${backgroundImage})`;
 
-        const weatherContainer = document.getElementById('weather-container');
-        weatherContainer.innerHTML = `
-          <h2>Current weather in ${city}, ${state}, ${country}</h2>
-          <p>Temperature: ${temperature.toFixed(1)} &#8451;</p>
-          <p>Weather: ${weatherDescription}</p>
-          <p>Humidity: ${humidity}%</p>
-          <p>Wind speed: ${windSpeed} m/s</p>
-          <img src="${weatherIcon}" alt="${weatherDescription}">
-        `;
+          weatherContainer.innerHTML = `
+            <h2>Current weather in ${city}</h2>
+            <p>Temperature: ${temperature.toFixed(1)} &#8451;</p>
+            <p>Weather: ${weatherDescription}</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Wind speed: ${windSpeed} m/s</p>
+            <img src="${weatherIcon}" alt="${weatherDescription}">
+          `;
       })
       .catch(error => {
         console.error(error);
@@ -53,4 +57,5 @@ fetch(apiUrl)
     console.error(error);
     const errorContainer = document.getElementById('error-container');
     errorContainer.innerHTML = '<p>Failed to fetch weather data. Please try again later.</p>';
-  });
+  })
+})
