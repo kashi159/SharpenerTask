d3320e603a33557171a63513025220f1090aa6af
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-17 22:01:42 +0530

    Done few Changes

--- Project1/version1.1/Weather/weather.js
+++ Project1/version1.1/Weather/weather.js
@@ -1,19 +1,32 @@
 const weatherInfo = document.getElementById('info')
 function fetchData(){
     return new  Promise((resolve, reject)=>{
-        // fetch('https://api.openweathermap.org/data/2.5/weather?lat=26.1667&lon=85.9&appid=aca8cb12e2f477338eec7f9249864994')
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=darbhanga&appid=aca8cb12e2f477338eec7f9249864994')
         // fetch('http://api.weatherapi.com/v1/current.json?key=5d2bd095ab6e4219a55155936231102&q=India&aqi=yes')
-        fetch('https://api.open-meteo.com/v1/forecast?latitude=26.1542&longitude=85.8918&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
+        // fetch('https://api.open-meteo.com/v1/forecast?latitude=26.1542&longitude=85.8918&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
         .then(response => response.json())
-        .then(data => resolve(data.current_weather));
+        .then(data => resolve(data));
         //.current.condition.text
     })
 }
 
-function display(weather) {
-    result=JSON.stringify(weather);
-    current= result.split('" "')
-        weatherInfo.innerHTML = `<h1>Weather of Darbhanga: ${current}</h1>`
+function display(data) {
+      const city = "Darbhanga"
+      const temperature = data.main.temp - 273.15; // Convert from Kelvin to Celsius
+      const feelLike = data.main.feels_like - 273.15;
+      const weatherDescription = data.weather[0].description;
+      const weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+       weatherInfo.innerHTML = `
+            <h2>Current weather in ${city}</h2>
+            <p>Temperature: ${temperature.toFixed(1)} &#8451;</p>
+            <p>Feels Like: ${feelLike.toFixed(1)} &#8451;</p>
+            <p>Weather: ${weatherDescription}</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Wind speed: ${windSpeed} m/s</p>
+            <img src="${weatherIcon}" alt="${weatherDescription}">
+          `;
         // weatherInfo.style.background= "grey";
         weatherInfo.style.width="Auto";
         // weatherInfo.style.fontSize="20px";
