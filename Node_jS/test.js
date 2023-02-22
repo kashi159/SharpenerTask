a862e3f7641bf7cf14ecdd857134b77990481762
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-22 22:44:23 +0530

    added

---
+++ Node_jS/test.js
@@ -0,0 +1,34 @@
+// const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+// console.log(array.map(item =>{
+//     if(item === ' '){
+//         item = 'empty string'
+//     }
+//     return item
+// }))
+
+// const obj1 = {'key1': 1}
+
+// const obj2 = { ...obj1}
+
+// if(obj2 === obj1){
+
+// console.log('same objects')
+
+// }
+
+// else{
+
+// console.log('different objects')
+
+// }
+
+const obj1 = {'key1': 1 , 'key2' : 2}
+
+const obj2 = { ...obj1, key1: 1000}
+
+
+
+console.log(obj1)
+
+console.log(obj2)
