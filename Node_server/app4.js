90a27ad9f89271bd8c3a5ee425cecb6016c0bb03
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-23 18:39:51 +0530

    Added routes.js file to export code to app4.js

---
+++ Node_server/app4.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const routes = require('./routes')
+
+console.log(routes.someText)
+const server = http.createServer(routes.handler);
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000.');
+});
