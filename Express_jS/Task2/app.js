707fa726681e0ea6553b3e267b1e4d0b9813c64a
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-24 21:59:20 +0530

    minor changes

--- Express_jS/Task2/app.js
+++ Express_jS/Task2/app.js
@@ -13,10 +13,10 @@ app.use('/add-product', (req, res, next)=> {
 app.post('/product',(req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
-})
+});
 
 app.use('/', (req, res, next)=> {
-    res.send('<h1>Hello from Express!</h1>')
-})
+    res.send('<h1>Hello from Express!</h1>');
+});
 
 app.listen(4000);       
