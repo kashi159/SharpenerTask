4895eac004c1d4102eab29f2d1f098c2a76258ba
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-25 17:39:09 +0530

    Task 6 commit added send file to serve html pages in views folder, added css using static path, used helper function path.dir

--- Express_jS/Task2/app.js
+++ Express_jS/Task2/app.js
@@ -1,4 +1,4 @@
-
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,13 +8,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes)
 
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views', 'error.html'));
 })
 
 app.listen(4000);       
