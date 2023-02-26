46e711c1394a1a1d9ef25525d45bd115953c5be9
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-02-26 12:15:28 +0530

    Task 7 commit added error404 page

--- Express_jS/Task2/app.js
+++ Express_jS/Task2/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorController = require('./controllers/error404')
 
 const app = express();
 
@@ -18,8 +19,6 @@ app.use(contactRoutes);
 app.use(successRoutes)
 
 
-app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views', 'error.html'));
-})
+app.use(errorController.get404)
 
 app.listen(4000);       
