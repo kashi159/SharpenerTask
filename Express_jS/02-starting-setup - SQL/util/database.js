a287513f6f3bbac3d53fcf54686107679e2dd770
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-03-02 10:41:24 +0530

    Task 17 commit,  added sequelize to do all the functionality with the help of SQL database.

--- Express_jS/02-starting-setup - SQL/util/database.js
+++ Express_jS/02-starting-setup - SQL/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Kashif@125'
-});
+const sequelize = new Sequelize('node-complete', 'root', 'Kashif@125', {
+    dialect: 'mysql',
+     host: 'localhost'
+    });
 
-module.exports = pool.promise();
+    module.exports = sequelize;
