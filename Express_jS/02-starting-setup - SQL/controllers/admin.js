bf5bfec20c636d914a96d91d534bff7cd86b4541
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-03-07 12:23:06 +0530

    Task 21 commit Designed the User and Product relationship

--- Express_jS/02-starting-setup - SQL/controllers/admin.js
+++ Express_jS/02-starting-setup - SQL/controllers/admin.js
@@ -13,14 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
   })
   .then(result => {
-    console.log(result);
+    // console.log(result);
     res.redirect('/admin/products')
   })
   .catch(err=>{
@@ -35,8 +35,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product =>{
+  req.user.getProducts({ where: {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products =>{
+    const product = products[0];
     if(!product){
       return res.redirect('/');
     }
@@ -85,7 +87,7 @@ exports.postDeleteProduct = (req, res, next)=>{
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+ req.user.getProducts()
   .then((products)=>{
     res.render('admin/products', {
       prods: products,
