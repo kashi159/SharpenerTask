d17a92e6cd34e275d51da271e0cc5f02617fe2ed
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-03-08 13:24:05 +0530

    Task 6 Commit Your user's password is at risk!!!- Added bcrypt in signup and login to secure password

--- ExpenseTracker_NodeJS-Project/Backend/controller/user.js
+++ ExpenseTracker_NodeJS-Project/Backend/controller/user.js
@@ -1,27 +1,33 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
-exports.postSignUpUser = (req, res, next) => {
+exports.postSignUpUser = async (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({ where: { email: email } }).then(user => {
+    try{
+        const user = await User.findOne({ where: { email: email } })
         if (user) {
             return res.status(409).json({ error: "User already exists" });
         }
-        User.create({
-            name: name,
-            email: email,
-            password: password
-        }).then(result => {
-            return res.json(result);
-        }).catch(err => {
-            console.error(err);
-            return res.status(500).json({ error: "Internal Server Error" });
-        });
-    }).catch(err => {
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, async (err, hash)=>{
+            if(err){
+              console.log(err);  
+            }
+            const result= await User.create({
+                name: name,
+                email: email,
+                password: hash
+            })
+                return res.json(result);
+        })
+       
+    }
+    catch(err) {
         console.error(err);
         return res.status(500).json({ error: "Internal Server Error" });
-    });
+    };
 }
 
 exports.postLoginUser = async (req, res, next) =>{
@@ -31,7 +37,8 @@ exports.postLoginUser = async (req, res, next) =>{
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
-      if (user.password !== password) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
         return res.status(401).json({ error: 'Incorrect password' });
       }
       return res.status(200).json(user);
