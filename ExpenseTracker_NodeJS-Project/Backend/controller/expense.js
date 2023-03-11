44a05b80adc7c77c7ddedefb7cc2f48325a6264e
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-03-11 10:04:18 +0530

    Added upadate to totalExpense when editing expense

--- ExpenseTracker_NodeJS-Project/Backend/controller/expense.js
+++ ExpenseTracker_NodeJS-Project/Backend/controller/expense.js
@@ -62,43 +62,53 @@ exports.deleteUserExpense = async (req, res, next) => {
   };
   
 
-exports.getEditExpense = (req, res, next) =>{
+exports.getEditExpense = async(req, res, next) =>{
     const prodId = req.params.id;
     const UserId = req.user.id;
-    UserExpense.findOne({
-        where: {
-            id: prodId,
-            userId: UserId
-        }
-    })
-    .then(expense =>{
-        return res.json(expense)
-    })
-    .catch(err => console.log(err))
+    try{
+        const expense = await UserExpense.findOne({
+            where: {
+                id: prodId,
+                userId: UserId
+            }
+        })
+        await User.decrement('totalExpense', {
+            by: expense.amount,
+            where: { id: req.user.id }
+          });
+       return res.json(expense)
+    }
+    catch(err) {console.log(err)}
 }
 
-exports.postEditExpense = (req, res, next) => {
-    const amount = req.body.amount;
-    const description = req.body.description;
-    const category = req.body.category
-    const prodId = req.params.id;
-    const UserId = req.user.id;
-    UserExpense.findOne({
+exports.postEditExpense = async (req, res, next) => {
+    try {
+      const amount = req.body.amount;
+      const description = req.body.description;
+      const category = req.body.category;
+      const prodId = req.params.id;
+      const UserId = req.user.id;
+      const expense = await UserExpense.findOne({
         where: {
-            id: prodId,
-            userId: UserId
-        }
-    }).then(expense =>{
-        // console.log(expense)
-        expense.update({
-            amount: amount,
-            description: description,
-            category: category
-        })
-        
-        return expense.save()
-    }).then(result =>{
-        return res.json(result)
-    })
-    .catch(err => console.log(err))
-}
+          id: prodId,
+          userId: UserId,
+        },
+      });
+      if (!expense) {
+        return res.status(404).json({ message: "Expense not found" });
+      }
+      expense.amount = amount;
+      expense.description = description;
+      expense.category = category;
+      const updatedExpense = await expense.save();
+      await User.increment('totalExpense', {
+        by: amount,
+        where: { id: req.user.id }
+      });
+      return res.json(updatedExpense);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
+  
