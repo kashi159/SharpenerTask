6497c99a5e1e91099bbffdb85a2b29077e867472
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-03-11 11:48:41 +0530

    Task 13 commit Making Expense App Production ready. Added transaction for post, delete and put method

--- ExpenseTracker_NodeJS-Project/Backend/controller/expense.js
+++ ExpenseTracker_NodeJS-Project/Backend/controller/expense.js
@@ -1,5 +1,6 @@
 const UserExpense = require('../models/expense');
-const User = require('../models/user')
+const User = require('../models/user');
+const sequelize = require('../util/userDatabase');
 
 exports.getUserExpense =  (req, res, next) => {
     const userId = req.user.id
@@ -16,30 +17,41 @@ exports.postUserExpense = async (req, res, next) => {
     const description = req.body.description;
     const category = req.body.category;
     const userId = req.user.id
+    let transact;
     try {
+      transact = await sequelize.transaction();
+      if(amount === undefined || amount.length ===0){
+        return res.status(400).json({ success: false, message: 'Parameters missing '})
+      }
         const result = await UserExpense.create({
             amount: amount,
             description: description,
             category: category,
-            userId: req.user.id
-        });
+            userId: userId
+        }, { transaction: transact});
         await User.increment('totalExpense', {
             by: amount,
-            where: { id: req.user.id }
+            where: { id: userId},
+            transaction: transact
           });
-
+          await transact.commit();
         return res.json(result);
     } catch (err) {
+      if (transact) {
+        await transact.rollback();
+      }
         console.log(err);
     }
 };
 
 
 exports.deleteUserExpense = async (req, res, next) => {
+    let transact;
     try {
+      transact = await sequelize.transaction();
       const prodId = req.params.id;
       const UserId = req.user.id;
-      console.log(req.user)
+      // console.log(req.user)
       const expense = await UserExpense.findOne({
         where: {
           id: prodId,
@@ -52,10 +64,15 @@ exports.deleteUserExpense = async (req, res, next) => {
       await expense.destroy();
       await User.decrement('totalExpense', {
         by: expense.amount,
-        where: { id: req.user.id }
+        where: { id: req.user.id },
+        transaction: transact
       });
+      await transact.commit();
       return res.status(204).end();
     } catch (err) {
+      if (transact) {
+        await transact.rollback();
+      }
       console.log(err);
       return res.status(500).json({ error: 'Internal server error' });
     }
@@ -65,7 +82,9 @@ exports.deleteUserExpense = async (req, res, next) => {
 exports.getEditExpense = async(req, res, next) =>{
     const prodId = req.params.id;
     const UserId = req.user.id;
+    let transact;
     try{
+        transact = await sequelize.transaction();
         const expense = await UserExpense.findOne({
             where: {
                 id: prodId,
@@ -74,15 +93,24 @@ exports.getEditExpense = async(req, res, next) =>{
         })
         await User.decrement('totalExpense', {
             by: expense.amount,
-            where: { id: req.user.id }
+            where: { id: req.user.id },
+            transaction: transact
           });
-       return res.json(expense)
+          await transact.commit();
+          return res.json(expense)
     }
-    catch(err) {console.log(err)}
+    catch(err) {
+      if (transact) {
+        await transact.rollback();
+      }
+      console.log(err)
+    }
 }
 
 exports.postEditExpense = async (req, res, next) => {
+  let transact;
     try {
+      transact = await sequelize.transaction();
       const amount = req.body.amount;
       const description = req.body.description;
       const category = req.body.category;
@@ -103,10 +131,15 @@ exports.postEditExpense = async (req, res, next) => {
       const updatedExpense = await expense.save();
       await User.increment('totalExpense', {
         by: amount,
-        where: { id: req.user.id }
+        where: { id: req.user.id },
+        transaction: transact
       });
+      await transact.commit();
       return res.json(updatedExpense);
     } catch (error) {
+      if (transact) {
+        await transact.rollback();
+      }
       console.log(error);
       return res.status(500).json({ message: "Internal server error" });
     }
