e1f6149c47ad6e62e1bd14c8bee90523c1628e7b
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-05-10 17:43:58 +0530

    Task 8 Commit CRUD operation using mongoose

--- MongoDb(NoSql)/00-starting-setup/controllers/admin.js
+++ MongoDb(NoSql)/00-starting-setup/controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const userId = req.user._id;
-  const product = new Product(title, price, description, imageUrl, null, userId);
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl
+  });
   product
     .save()
     .then(result => {
@@ -55,10 +59,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  
-     const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
-     product.save()
-      .then(result => {
+    
+    Product.findById(prodId).then(product =>{
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+
+      return product.save()
+    })
+    .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
@@ -67,7 +77,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product
-    .fetchAll()
+    .find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -80,10 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-    .then(product => {
-      console.log(product)
-    })
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
